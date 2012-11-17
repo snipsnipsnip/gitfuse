ba10899205c536b7d07ead5099d7e65797dd4115
David Sanders <dsanders@fusionbox.com>
2012-11-17 12:23:49 -0700

    Almost ready...

--- fusepy_gitfs.py
+++ fusepy_gitfs.py
@@ -5,35 +5,28 @@ import argparse
 import errno
 import os
 import stat
-import fuse
 import pygit2
 import sys
+import logging
 
 from collections import namedtuple
 from fuse import FuseOSError, FUSE, Operations, LoggingMixIn
 
 
-arguments = argparse.ArgumentParser(
-    description='Mounts the contents of a git repository in read-only mode using FUSE.'
-)
-arguments.add_argument('repo', metavar='repo', help='Path to git repository.')
-arguments.add_argument('mount', metavar='mount', help='Path to mount point.')
-
-
 StatT = namedtuple('StatT', ['st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid', 'st_gid', 'st_size', 'st_atime', 'st_mtime', 'st_ctime'])
-"""
-st_mode:  protection bits
-st_ino:   inode number
-st_dev:   device
-st_nlink: number of hard links
-st_uid:   user ID of owner
-st_gid:   group ID of owner
-st_size:  size of file, in bytes
-st_atime: time of most recent access
-st_mtime: time of most recent content modification
-st_ctime: platform dependent; time of most recent metadata change on Unix,
-          or the time of creation on Windows
-"""
+
+##|  st_mode:  protection bits
+##|  st_ino:   inode number
+##|  st_dev:   device
+##|  st_nlink: number of hard links
+##|  st_uid:   user ID of owner
+##|  st_gid:   group ID of owner
+##|  st_size:  size of file, in bytes
+##|  st_atime: time of most recent access
+##|  st_mtime: time of most recent content modification
+##|  st_ctime: platform dependent; time of most recent metadata change on Unix,
+##|            or the time of creation on Windows
+
 stat_zero = StatT(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 
@@ -44,35 +37,51 @@ def copy_stat(st, **kwargs):
 
 def git_tree_to_direntries(tree):
     for entry in tree:
-        obj = entry.to_object()
-        stat_type = stat.S_IFDIR if isinstance(obj, pygit2.Tree) else stat.S_IFREG
-        yield fuse.Direntry(entry.name.encode('utf-8'), type=stat_type)
+        yield entry.name.encode('utf-8')
 
 
 def git_tree_find_recursive(tree, path):
     parts = path.split('/')
-    tree = reduce(lambda t, name: (t[name].to_object() if t is not None else None),
-                  parts[:-1], tree)
+
+    tree = reduce(
+        lambda t, name: (t[name].to_object() if t is not None else None),
+        parts[:-1],
+        tree,
+    )
+
     if tree is None:  # dir1
         return None
+
     entry = tree[parts[-1]]
     return entry
 
 
 class GitFS(Operations, LoggingMixIn):
-    def __init__(self, dir):
-        dir = os.path.abspath(dir)
-        dot_git = os.path.join(dir, '.git')
+    class GitFSError(Exception):
+        pass
 
-        self.dir = dot_git if os.path.exists(dot_git) else dir
+    def __init__(self, git_path):
+        git_path = os.path.abspath(git_path)
+        dot_git = os.path.join(git_path, '.git')
 
-        self.repo = pygit2.Repository(self.git_dir)
+        if not (os.path.exists(git_path) or os.path.exists(dot_git)):
+            raise self.GitFSError('Neither \'{0}\' or \'{1}\' exists'.format(
+                git_path,
+                dot_git,
+            ))
+
+        self.repo = pygit2.Repository(
+            dot_git if os.path.exists(dot_git) else git_path
+        )
 
     def getattr(self, path, fh=None):
         stat_repo = os.lstat(self.repo.path)
-        default_stat_dir = copy_stat(stat_repo, st_ino=0,
-                                     # This is read-only file system
-                                     st_mode=stat_repo.st_mode & ~0222)
+
+        default_stat_dir = copy_stat(
+            stat_repo, st_ino=0,
+            # This is read-only file system
+            st_mode=stat_repo.st_mode & ~0222,
+        )
 
         if path == '/':
             return default_stat_dir
@@ -102,6 +111,7 @@ class GitFS(Operations, LoggingMixIn):
 
             blob = self.repo[entry.oid]
             size = len(blob.data)
+
             # This is read-only file system
             mode = entry.attributes & ~0222
             return copy_stat(stat_repo, st_ino=0, st_size=size, st_mode=mode)
@@ -109,101 +119,82 @@ class GitFS(Operations, LoggingMixIn):
         raise FuseOSError(errno.ENOENT)
 
     def readdir(self, path, offset):
-        refs = [s[4:].encode('utf-8') for s in self.repo.listall_references() if s.startswith("refs/")]
+        refs = [s[4:].encode('utf-8') for s in self.repo.listall_references() if s.startswith('refs/')]
 
         # Special case
-        if path == "/":
-            return list(frozenset([parts[1] for parts in [ref.split("/") for ref in refs] if len(parts) > 0]))
+        if path == '/':
+            return list(frozenset([parts[1] for parts in [ref.split('/') for ref in refs] if len(parts) > 0]))
 
         # Path is a strict parent of a ref? Example: /remotes
         path_len_1 = len(path) + 1
-        matching = [ref for ref in refs if ref.startswith(path + "/")]
+        matching = [ref for ref in refs if ref.startswith(path + '/')]
         if len(matching) > 0:
-            children = frozenset([ref[path_len_1:].split("/", 1)[0] for ref in matching if len(ref) > path_len_1])
-            return [fuse.Direntry(name, type=stat.S_IFDIR) for name in children]
+            return list(frozenset([ref[path_len_1:].split('/', 1)[0] for ref in matching if len(ref) > path_len_1]))
 
         # Path is ref? Example: /heads/master
         if path in refs:
-            ref = self.repo.lookup_reference("refs" + path)
+            ref = self.repo.lookup_reference('refs' + path)
             ref = ref.resolve()
             commit = self.repo[ref.oid]
             return list(git_tree_to_direntries(commit.tree))
 
         # Path is strict child of a ref? Example: /heads/master/dir1/subdir
-        matching = [ref for ref in refs if path.startswith(ref + "/")]
+        matching = [ref for ref in refs if path.startswith(ref + '/')]
         if len(matching) == 1:
             ref_name = matching[0]  # /heads/master
-            ref = self.repo.lookup_reference("refs" + ref_name)
+            ref = self.repo.lookup_reference('refs' + ref_name)
             commit = self.repo[ref.oid]
             file_path = path[len(ref_name) + 1:]  # dir1/subdir
             entry = git_tree_find_recursive(commit.tree, file_path)
             if entry is None:
-                return -errno.ENOENT
+                raise FuseOSError(errno.ENOENT)
             if entry.attributes & stat.S_IFDIR == stat.S_IFDIR:
                 subtree = self.repo[entry.oid]
                 return list(git_tree_to_direntries(subtree))
 
-        log.debug(u"  Fallback")
         return []
 
     def open(self, path, flags):
-        log.debug(u"open(%s, %s)", path, flags)
-
-        if path.startswith("/."):
-            return -errno.ENOENT
+        if path.startswith('/.'):
+            return FuseOSError(errno.ENOENT)
 
         if flags & os.O_RDONLY != os.O_RDONLY:
-            return -errno.EACCES
+            return FuseOSError(errno.EACCES)
 
     def read(self, path, size, offset):
-        log.debug(u"read(%s, %s, %s)", path, size, offset)
-        if path.startswith("/."):
-            return -errno.ENOENT
+        if path.startswith('/.'):
+            return FuseOSError(errno.ENOENT)
 
-        refs = [s[4:].encode('utf-8') for s in self.repo.listall_references() if s.startswith("refs/")]
+        refs = [s[4:].encode('utf-8') for s in self.repo.listall_references() if s.startswith('refs/')]
 
         # Path is strict child of a ref? Example: /heads/master/README.txt
-        matching = [ref for ref in refs if path.startswith(ref + "/")]
+        matching = [ref for ref in refs if path.startswith(ref + '/')]
         if len(matching) == 1:
             ref_name = matching[0]  # /heads/master
             file_path = path[len(ref_name) + 1:]  # README.txt
-            ref = self.repo.lookup_reference("refs" + ref_name)
+            ref = self.repo.lookup_reference('refs' + ref_name)
             commit = self.repo[ref.oid]
             entry = git_tree_find_recursive(commit.tree, file_path)
             if entry is None:
-                return -errno.ENOENT
+                return FuseOSError(errno.ENOENT)
             blob = entry.to_object()
             if offset == 0 and len(blob.data) <= size:
                 return blob.data
             return blob.data[offset:offset + size]
 
-        log.debug(u"  Fallback")
-        return -errno.ENOENT
+        return FuseOSError(errno.ENOENT)
 
 
-def main(arguments):
-    server = GitFS(arguments.repo)
-    server.flags = 0
-    server.multithreaded = 0
+if __name__ == '__main__':
+    arguments = argparse.ArgumentParser(
+        description='Mounts the contents of a git repository in read-only mode using FUSE.'
+    )
+    arguments.add_argument('git_path', metavar='<git_path>', help='Path to git repository.')
+    arguments.add_argument('mount_path', metavar='<mount_path>', help='Path to mount point.')
 
-    fuse_args = [arguments.mount, "-f"]
-    if arguments.fuse:
-        fuse_args += ["-o", arguments.fuse]
-    server.parse(fuse_args, errex=1)
+    if len(sys.argv) != 3:
+        arguments.print_help()
+        sys.exit(0)
 
-    if server.fuse_args.mount_expected():
-        if not os.path.exists(server.fuse_args.mountpoint):
-            print >>sys.stderr, u"Mount point '{0}' does not exist.".format(server.fuse_args.mountpoint)
-            exit(2)
-    return server.main()
-
-
-if __name__ == "__main__":
-    self_pid = os.getpid()
-    signal.signal(signal.SIGINT, lambda *a, **kw: os.kill(self_pid, signal.SIGTERM))
-    logging.basicConfig(level=logging.WARN)
-    log = logging.getLogger("gitfs")
-    try:
-        exit(main(arguments.parse_args()))
-    except KeyboardInterrupt:
-        exit(1)
+    logging.getLogger().setLevel(logging.DEBUG)
+    # fuse = FUSE(GitFS(arguments.git_path), arguments.mount_path, foreground=True)
